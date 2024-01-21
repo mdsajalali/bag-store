d38fa1ccee65158b5849d19c46c30f906726571a
mdsajalali <mdsajalali2020@gmail.com>
2024-01-21 17:56:56 +0600

    navbar code modify

---
+++ src/components/NavIcons.tsx
@@ -0,0 +1,23 @@
+import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
+
+const NavIcons = () => {
+  return (
+    <div className="flex items-center sm:gap-7 gap-4 md:pr-4">
+      <FaSearch
+        size={20}
+        className="hover:text-[#515151] cursor-pointer transition-all"
+      />
+      <FaUserCircle
+        size={20}
+        className="hover:text-[#515151] cursor-pointer transition-all"
+      />
+      <FaBagShopping
+        size={20}
+        className="hover:text-[#515151] cursor-pointer transition-all"
+      />
+    </div>
+  );
+};
+
+export default NavIcons;
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { FaSearch, FaUserCircle } from "react-icons/fa";
-import { FaBagShopping } from "react-icons/fa6";
+
 import { GrMenu } from "react-icons/gr";
 import { IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import NavItems from "../components/NavItems";
+import NavIcons from "../components/NavIcons";
 
 const Navbar: React.FC = () => {
   const [isClick, setIsClick] = useState<boolean>(false);
@@ -15,29 +15,20 @@ const Navbar: React.FC = () => {
 
   return (
     <div className="flex items-center justify-between my-4">
+      {/* Navbar Logo */}
       <div>
         <h1 className="sm:text-3xl text-2xl font-bold pl-4 xl:pl-0">
           <Link to="/">Bag Store .</Link>
         </h1>
       </div>
-      
+
       {/* navItems */}
       <NavItems isClick={isClick} />
 
-      <div className="flex items-center sm:gap-7 gap-4 md:pr-4">
-        <FaSearch
-          size={20}
-          className="hover:text-[#515151] cursor-pointer transition-all"
-        />
-        <FaUserCircle
-          size={20}
-          className="hover:text-[#515151] cursor-pointer transition-all"
-        />
-        <FaBagShopping
-          size={20}
-          className="hover:text-[#515151] cursor-pointer transition-all"
-        />
-      </div>
+      {/* navIcons */}
+      <NavIcons />
+
+      {/* navBar Toggle */}
       <div onClick={handleClick} className="md:hidden pr-4 md:pr-0">
         {isClick ? (
           <IoClose
