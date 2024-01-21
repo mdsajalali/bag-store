543204b90185b2b520b3b3243d3f776a453bc3e3
mdsajalali <mdsajalali2020@gmail.com>
2024-01-21 16:33:13 +0600

    navbar design added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,11 +3,11 @@ import Navbar from "../Navbar";
 
 const Home = () => {
   return (
-    <>
+    <div className="shadow-md py-1">
       <Container>
         <Navbar />
       </Container>
-    </>
+    </div>
   );
 };
 
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,12 +1,62 @@
-import React from "react";
+import { FaSearch, FaUserCircle } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
-const Navbar: React.FC = () => {
+const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between my-4 ">
       <div>
-        <h1>
-          Bag <span>Store</span>
+        <h1 className="text-3xl font-bold">
+          <Link to="/">Bag Store .</Link>
         </h1>
+      </div>
+      <nav className="flex list-none gap-5 ">
+        <li>
+          <Link
+            to="/"
+            className="text-[18px] font-semibold hover:bg-[#121212] hover:text-white py-2 px-3 transition-all"
+          >
+            Shop
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            className="text-[18px] font-semibold hover:bg-[#121212] hover:text-white py-2 px-3 transition-all"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            className="text-[18px] font-semibold hover:bg-[#121212] hover:text-white py-2 px-3 transition-all"
+          >
+            Faq
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/"
+            className="text-[18px] font-semibold hover:bg-[#121212] hover:text-white py-2 px-3 transition-all"
+          >
+            Contact
+          </Link>
+        </li>
+      </nav>
+      <div className="flex items-center gap-5">
+        <FaSearch
+          size={20}
+          className="hover:text-[#515151] cursor-pointer transition-all"
+        />
+        <FaUserCircle
+          size={20}
+          className="hover:text-[#515151] cursor-pointer transition-all"
+        />
+        <FaBagShopping
+          size={20}
+          className="hover:text-[#515151] cursor-pointer transition-all"
+        />
       </div>
     </div>
   );
