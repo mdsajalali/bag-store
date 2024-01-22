02b231e97c79cf9ca0262fa6bb542bdedf21f104
mdsajalali <mdsajalali2020@gmail.com>
2024-01-22 11:50:27 +0600

    bag card section cart icon added

--- src/components/BagCard.tsx
+++ src/components/BagCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { LuShoppingCart } from "react-icons/lu";
 
 interface BagCardProps {
   data: {
@@ -10,7 +11,10 @@ interface BagCardProps {
 
 const BagCard: React.FC<BagCardProps> = ({ data }) => {
   return (
-    <div className="shadow-md rounded-md p-4 hover:translate-y-2 transition-all cursor-pointer w-[250px]">
+    <div className="shadow-md rounded-md p-4 hover:translate-y-2 transition-all cursor-pointer w-[250px] xl:w-[280px]  relative">
+      <div className="bg-[#121212] absolute left-0 top-0 inline-block p-1 xl:p-2 text-white  ">
+        <LuShoppingCart size={20} />
+      </div>
       <img src={data?.img} alt="" />
       <div className="text-center">
         <p className="font-bold text-[18px] mb-2">{data?.desc}</p>
